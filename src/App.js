621c0844be8f4838e926f593900e3e71e9e1f481
robert5313 <kiptoorobert@etech.ac.ke>
2023-02-16 23:37:57 +0300

    my OpenAI study on how to consume openAI API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react"
+const { Configuration, OpenAIApi } = require("openai");
 
-function App() {
+const App = () => {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+  const openai = new OpenAIApi(configuration);
+  const [prompt, setPrompt] = useState("");
+  const [apiResponse, setApiResponse] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const result = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: prompt,
+        temperature: 0.5,
+        max_tokens: 4000,
+      });
+      //console.log("response", result.data.choices[0].text);
+      setApiResponse(result.data.choices[0].text);
+    } catch (e) {
+      //console.log(e);
+      setApiResponse("Something is going wrong, Please try again.");
+    }
+    setLoading(false);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: '100vh',
+        }}
+      >
+        <form onSubmit={handleSubmit}>
+          <textarea
+            type="text"
+            value={prompt}
+            placeholder="Please ask to openai"
+            onChange={(e) => setPrompt(e.target.value)}
+          ></textarea>
+          <button
+            disabled={loading || prompt.length === 0}
+            type="submit"
+          >
+            {loading ? "Generating..." : "Generate"}
+          </button>
+        </form>
+      </div>
+      {apiResponse && (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <pre>
+            <strong>API response:</strong>
+            {apiResponse}
+          </pre>
+        </div>
+      )}
+    </>
   );
-}
+};
+
 
 export default App;
